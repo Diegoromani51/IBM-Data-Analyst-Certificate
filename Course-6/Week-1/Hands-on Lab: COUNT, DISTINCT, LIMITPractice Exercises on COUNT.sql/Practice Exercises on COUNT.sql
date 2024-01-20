97f8022b3d13316fbc88b4c361fe5749a64d84b1
Diego Romani <139392370+Diegoromani51@users.noreply.github.com>
2024-01-20 14:13:59 -0800

    Delete Course-6/Week-1/Hands-on Lab: COUNT, DISTINCT, LIMITPractice Exercises on COUNT.sql directory

--- Course-6/Week-1/Hands-on Lab: COUNT, DISTINCT, LIMITPractice Exercises on COUNT.sql/Practice Exercises on COUNT.sql
+++
@@ -1,8 +0,0 @@
--- Problem 1: Retrieve the number of locations of the films which are directed by Woody Allen.
- SELECT COUNT(Locations) FROM FilmLocations WHERE Director="Woody Allen";
-
--- Problem 2: Retrieve the number of films shot at Russian Hill.
- SELECT Count(Title) FROM FilmLocations WHERE Locations="Russian Hill";
-
--- Problem 3: Retrieve the number of rows having a release year older than 1950 from the “FilmLocations” table.
- SELECT Count(*) FROM FilmLocations WHERE ReleaseYear<1950;
